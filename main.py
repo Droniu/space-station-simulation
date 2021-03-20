2307c7137c9246e5c264f22982bbbfca13b94fc8
Droniu <dron.official@yahoo.com>
2021-03-20 20:59:43 +0100

    player movement

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import pygame as pog
+import pygame
 import random as r
 import threading
 import time
 
 # todo - enemy class, medbay, weapons
 
-class Astronaut(threading.Thread):
+class Astronaut(threading.Thread, pygame.sprite.Sprite):
     def __init__(self, x=0, y=0, running=True):
         threading.Thread.__init__(self)
-        self.x = x
-        self.y = y
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("sprites/fighter_simple.gif")
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y
         self.running = running
         self.movementX = 0
         self.movementY = 0
@@ -17,39 +20,78 @@ class Astronaut(threading.Thread):
     def run(self):
         while self.running:
             time.sleep(0.05)
-            self.x += self.movementX
-            self.y += self.movementY
-        
+            self.rect.left += self.movementX
+            self.rect.top -= self.movementY
+            
+class Enemy():
+    def __init__(self, x=0, y=0, alive=True):
+        pass
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
             
             
 
 def main():
     
-    pog.init()
+    pygame.init()
 
-    screen = pog.display.set_mode([800, 450])
+    screen = pygame.display.set_mode([1280, 720])
 
     entity = Astronaut(x=250, y=250)
     entity.start()
 
+    bg = Background("sprites/map_draft.png", [0,0])
+    
     running = True
     while running:
 
-        for event in pog.event.get():
-            if event.type == pog.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 entity.running = False
                 running = False
-            if event.type == pog.MOUSEBUTTONUP:
-                entity.movementX = r.randint(-3, 3)
-                entity.movementY = r.randint(-3, 3)
+            """if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    entity.movementX -= 19
+                if event.key == pygame.K_RIGHT:
+                    entity.movementX += 19
+                if event.key == pygame.K_UP:
+                    entity.movementY += 19
+                if event.key == pygame.K_DOWN:
+                    entity.movementY -= 19
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP or pygame.K_DOWN:
+                    entity.movementY = 0
+                if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                    entity.movementX = 0
+            """
+        keys_pressed = pygame.key.get_pressed()
 
-        screen.fill((255, 40, 69))
-        pog.draw.circle(screen, (0, 0, 255), (entity.x, entity.y), 25)
+        if keys_pressed[pygame.K_LEFT]:
+            entity.movementX -= 1
+
+        if keys_pressed[pygame.K_RIGHT]:
+            entity.movementX += 1
+
+        if keys_pressed[pygame.K_UP]:
+            entity.movementY += 1
+
+        if keys_pressed[pygame.K_DOWN]:
+            entity.movementY -= 1
+
+
+        screen.fill([32, 32, 32])
+        screen.blit(bg.image, bg.rect)
+        screen.blit(entity.image, entity.rect)
         
 
-        pog.display.flip()
+        pygame.display.flip()
 
-    pog.quit()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
